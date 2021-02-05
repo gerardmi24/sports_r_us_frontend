e33b88482b07589138a463fa8f625c7ddde39429
gerardmi24 <gerardmistretta@yahoo.com>
2021-02-05 11:47:50 -0500

    useEffect

--- sports_r_us_frontend/src/App.js
+++ sports_r_us_frontend/src/App.js
@@ -1,14 +1,17 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 import Header from './Header';
 import DefaultHomePage from './DefaultHomePage';
 
-
 function App() {
-
   const [search, setSearch] = useState("")
-  
   // console.log("Search", search)
+
+  // useEffect(() => {
+  //   fetch()
+  //   .then(resp => resp.json())
+  //   .then((teams) => )
+  // })
   
   return (
     <div className="App">
