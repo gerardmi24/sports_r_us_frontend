89c3557b802ced483eaf3caddf4c762d1efdac4f
gerardmi24 <gerardmistretta@yahoo.com>
2021-02-06 15:39:17 -0500

    New pitch

--- sports_r_us_frontend/src/App.js
+++ sports_r_us_frontend/src/App.js
@@ -4,15 +4,23 @@ import Header from './Header';
 import DefaultHomePage from './DefaultHomePage';
 
 function App() {
+  const sports = "http://localhost:3000/sports"
+  const users = "http://localhost:3000/users"
+  const favorites = "http://localhost:3000/favorites"
+  const teams = "http://localhost:3000/teams"
   const [search, setSearch] = useState("")
+  const [allSports, setAllSports] = useState([])
   // console.log("Search", search)
-
-  // useEffect(() => {
-  //   fetch()
-  //   .then(resp => resp.json())
-  //   .then((teams) => )
-  // })
   
+  useEffect(() => {
+    fetch(sports)
+    .then(r => r.json())
+    .then((sports) => {
+      console.log(sports)
+      // setAllSports(sports)
+    });
+  }, []);
+
   return (
     <div className="App">
      <Header search={search} setSearch={setSearch} />
